9664151b7cba2be47595d4891ea9ccf8beebb78a
Josué Martell <josue.martell17@gmail.com>
2022-11-23 01:25:48 -0600

    feat: set fish theme depending on gtk preference

---
+++ conf.d/theme.fish
@@ -0,0 +1,8 @@
+# SET THEME #
+set current_gtk_theme (gsettings get org.gnome.desktop.interface color-scheme)
+
+if [ $current_gtk_theme = "'prefer-light'" ]
+	fish_config theme choose "Catppuccin Latte"
+else
+	fish_config theme choose "Catppuccin Mocha"
+end
