ec1eee78e57cd11852c9de08af99513f121971eb
Josué Martell <josue.martell17@gmail.com>
2022-11-22 23:55:15 -0600

    feat: add to PATH

--- config.fish
+++ config.fish
@@ -2,3 +2,10 @@
 if not functions -q fundle;
 	eval (curl -sfL https://git.io/fundle-install);
 end
+
+# PATH #
+fish_add_path -aP $HOME/.cargo/bin
+fish_add_path -aP $HOME/.local/bin
+fish_add_path -aP $HOME/.local/share/neovim/bin
+fish_add_path -aP $HOME/.local/share/pnpm
+
