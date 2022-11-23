80efb7529877829cfe88f4d47a4e0e977ecfe11c
Josué Martell <josue.martell17@gmail.com>
2022-11-23 01:24:17 -0600

    feat: change greeter to pokemon sprite

--- config.fish
+++ config.fish
@@ -9,3 +9,7 @@ fish_add_path -aP $HOME/.local/bin
 fish_add_path -aP $HOME/.local/share/neovim/bin
 fish_add_path -aP $HOME/.local/share/pnpm
 
+# POKEMON SPRITE GREETER #
+function fish_greeting
+  krabby random --no-gmax
+end
