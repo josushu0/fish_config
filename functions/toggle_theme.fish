6910943a6206c10dfc13e5add532d8a0d7325a79
Josué Martell <josue.martell@neoaddition.com>
2023-06-05 13:53:38 -0600

    use rose pine theme

--- functions/toggle_theme.fish
+++ functions/toggle_theme.fish
@@ -2,10 +2,10 @@
 function toggle_theme
 	if [ "$TERM" = xterm-kitty ]
 		set current_kitty_theme (awk '$1=="include" {print $2}' "$HOME/.config/kitty/kitty.conf")
-		set new_kitty_theme "catppuccin-mocha.conf"
+		set new_kitty_theme "rose-pine.conf"
 
-		if [ "$current_kitty_theme" = "catppuccin-mocha.conf" ]
-			set new_kitty_theme "catppuccin-latte.conf"
+		if [ "$current_kitty_theme" = "rose-pine.conf" ]
+			set new_kitty_theme "rose-pine-dawn.conf"
 		end
 
 		# Set theme for active sessions. Requires `allow_remote_control yes`
@@ -17,10 +17,10 @@ function toggle_theme
 
 	if [ "$TERM" = alacritty ]
 		set current_alacritty_theme (awk '$1=="-" {print $2}' "$HOME/.config/alacritty/alacritty.yml")
-		set new_alacritty_theme "catppuccin-mocha.yml"
+		set new_alacritty_theme "rose-pine.yml"
 
-		if [ "$current_alacritty_theme" = "~/.config/alacritty/catppuccin-mocha.yml" ]
-			set new_alacritty_theme "catppuccin-latte.yml"
+		if [ "$current_alacritty_theme" = "~/.config/alacritty/rose-pine.yml" ]
+			set new_alacritty_theme "rose-pine-dawn.yml"
 		end
 
 		# Update config for persistence
